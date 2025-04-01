30f5c7490c617115c978fd05310b6a5110235d9b
Gopika Janarthanan <141640423+Gopika-Janarthanan@users.noreply.github.com>
2025-04-01 13:05:36 +0530

    Update Main.java

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Edited from git");
-        System.out.println(" now is it okahy");
+        System.out.println(" now is it okay , let me check ");
 
     }
 }
