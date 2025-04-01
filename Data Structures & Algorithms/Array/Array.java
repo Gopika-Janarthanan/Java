31dafb141f2cb11b7d489a8f68166e3845b28e3b
Gopika Janarthanan <141640423+Gopika-Janarthanan@users.noreply.github.com>
2025-04-01 11:59:16 +0530

    Update Array.java

--- Data Structures & Algorithms/Array/Array.java
+++ Data Structures & Algorithms/Array/Array.java
@@ -1,1 +1,35 @@
+package Java_DSA_Course;
+
+public class Array {
+    public static void main(String[] args){
+        avgTemp(new int []{1,3,4,6,8});
+        System.out.println(missingNum(new int[]{1,2,3,5}));
+    }
+    static void avgTemp( int []arg ){
+        int length = arg.length;
+        int s=0;
+        for (int i : arg){
+            s+=i;
+
+        }
+        System.out.println("sum : "+s);
+        System.out.println("length : "+length);
+        System.out.println("avg : "+s/length);
+
+    }
+
+static int missingNum(int []arr){
+        int s=0;
+       int c=0;
+        int a;
+        int i;
+    for (i= 1; i < arr.length; i++) {
+        s+= arr[i];
+        c+=i;
+
+    }
+    a= s-c;
+    return a;
+}
+}
 
